9a4641aae0460c455cf68c43a4d585834e085a31
Ritu <27ritu.patel10@gmail.comgit config --global user.email 27ritu.patel10@gmail.com>
2022-05-20 16:07:30 +0530

    first commit

---
+++ components/SlidebarOptions.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Add } from "@mui/icons-material";
+import './CSS/SidebarOptions.css';
+
+const SidebarOptions = () => {
+    return (
+      <div className="sidebarOptions">
+        <div className="sidebarOption">
+          <img
+            src="https://qphs.fs.quoracdn.net/main-thumb-t-930-100-cbbsbwijdhpyzlpipejvqpiijhhoaday.jpeg"
+            alt=""
+          />
+          <p>History</p>
+        </div>
+  
+        <div className="sidebarOption">
+          <img
+            src="https://qphs.fs.quoracdn.net/main-thumb-t-858-100-VnZbEVtOIGkEHXlnYId9slumV59IPgkA.jpeg"
+            alt=""
+          />
+  
+          <p>Business</p>
+        </div>
+        <div className="sidebarOption">
+          <img
+            src="https://qphs.fs.quoracdn.net/main-thumb-t-1913-100-B8JrwaVauFzsaTSqXDqoWLCXzQb2mTE9.jpeg"
+            alt=""
+          />
+          <p>Psychology</p>
+        </div>
+  
+        <div className="sidebarOption">
+          <img
+            src="https://qphs.fs.quoracdn.net/main-thumb-t-877-100-e7jKHEQr0HExAIA9rlsyHlV6HJyRruEo.jpeg"
+            alt=""
+          />
+          <p>Cooking</p>
+        </div>
+  
+        <div className="sidebarOption">
+          <img
+            src="https://qphs.fs.quoracdn.net/main-thumb-t-801-100-Sf8h894FXbQZQit0TeqDrrqS6xw6dwCQ.jpeg"
+            alt=""
+          />
+          <p>Music</p>
+        </div>
+  
+        <div className="sidebarOption">
+          <img
+            src="https://qphs.fs.quoracdn.net/main-thumb-t-931-100-c8WCPwZ9qPsh5zLGQ5wHh1ddxtc9Cch7.jpeg"
+            alt=""
+          />
+          <p>Science</p>
+        </div>
